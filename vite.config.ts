cfac7fcf022f9f1134568e6f447735c442ad0c9a
as <allensnape@qq.com>
2023-12-23 20:42:24 +0800

    chore: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const basePath = process.env.APP_BASE_PATH || undefined;
+const outDir = process.env.APP_OUTDIR || 'dist';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:  '/docs',
+  base:  basePath,
   build: {
-    outDir: 'docs',
+    outDir,
   },
   plugins: [react()],
 });
