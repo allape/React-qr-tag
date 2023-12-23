6de9a0cce72ad017bc5775f87798308066bcc84b
as <allensnape@qq.com>
2023-12-23 20:33:31 +0800

    refactor: refine for docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build:   {
+  base:  '/docs',
+  build: {
     outDir: 'docs',
   },
+  plugins: [react()],
 });
